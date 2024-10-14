89b2b07823c774cb84c0db02ad09463a4fc28368
Farzine <farzine07@student.sust.edu>
2024-10-15 01:20:06 +0600

    again revoking commit

--- server2/controllers/auth/auth-controller.js
+++ server2/controllers/auth/auth-controller.js
@@ -67,7 +67,14 @@ const loginUser = async (req, res) => {
       { expiresIn: "24h" }
     );
 
-    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+    res.cookie("token", token, 
+      { 
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "None",
+        path: '/',
+        maxAge: 24 * 60 * 60 * 1000, 
+      }).json({
       success: true,
       message: "Logged in successfully",
       user: {
